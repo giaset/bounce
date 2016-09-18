cb72a7a84191f3a799a4dec400969309d842db4c
Gianni Settino <g@miltonandparc.com>
2016-09-18 19:34:27 -0400

    wip

--- bounce/ViewController.swift
+++ bounce/ViewController.swift
@@ -6,20 +6,30 @@
 //  Copyright © 2016 Milton and Parc. All rights reserved.
 //
 
+import CoreMotion
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let motionManager = CMMotionManager()
+    private let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.textColor = UIColor.white
+        label.numberOfLines = 0
+        view.addSubview(label)
+        
+        motionManager.deviceMotionUpdateInterval = 0.01
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (data: CMDeviceMotion?, error: Error?) in
+            if let acceleration = data?.userAcceleration {
+                self.label.text = "x = \(acceleration.x)\ny = \(acceleration.y)\nz = \(acceleration.z)"
+                self.label.sizeToFit()
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
-
-
 }
-
